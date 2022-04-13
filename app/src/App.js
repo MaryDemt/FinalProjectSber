cf0133eeabb15bde8a3183650155942f31284fee
MaryDemt <mary.shevtcova@yandex.ru>
2022-04-13 22:00:28 +0300

    Реализован функционал добавления поста

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import Container from '@mui/material/Container';
+import NavBar from './components/NavBar/NavBar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Main from './components/Main/Main';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const themeLight = createTheme({
+  palette: {
+    primary: {
+      main: '#0052cc',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={themeLight}>
+      <CssBaseline />
+    <BrowserRouter>
+      <NavBar />
+      <Container maxWidth="md" className="container">
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ app/src/components/PostsList/PostsList.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadAllPosts } from "../../redux/actions/postsAC";
+import PostItem from "../PostItem/PostItem";
+import Grid from "@mui/material/Grid";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector((store) => store.posts);
+
+  useEffect(() => {
+    dispatch(loadAllPosts());
+  }, []);
+
+  console.log({ posts });
+
+  if (!posts.length) return <div>Posts list is empty</div>;
+
+  return (
+    <Grid container spacing={2} justifyContent="center">
+      {posts.map((post) => {
+        return <PostItem key={post._id} {...post} />;
+      })}
+    </Grid>
+  );
+};
+
+export default PostsList;
