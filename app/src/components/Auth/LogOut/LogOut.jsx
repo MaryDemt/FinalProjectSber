7376a237cbebba9846ff4ef95cf1c73f998220fa
smska2022 <2009sms@rambler.ru>
2022-04-21 22:29:19 +0300

    Исправленная регистрация

--- app/src/components/Auth/LogOut/LogOut.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, useLocation } from "react-router-dom";
-
-export const LogOut = () => {
-    localStorage.clear()
-    let location = useLocation();
-    return <Navigate to="/signin" state={{ from: location }} replace />;
-}
